6083c6bfbfecd061e460e9b16fdb9e788b0c558c
Jihwan Jang <jjh0302@gmail.com>
2024-06-29 23:06:41 +0900

    Update for exited container situation

--- startServer.sh
+++ startServer.sh
@@ -1,2 +1,21 @@
+#!/bin/bash
+
+# check work folder
 [ ! -d "work" ] && mkdir work
-docker run --name gpu-jupyter-docker --gpus all -d -it -p 8848:8888 -v $(pwd)/work:/home/jovyan/work -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --user root cschranz/gpu-jupyter:v1.7_cuda-12.2_ubuntu-22.04
+
+# declare container name
+CONTAINER_NAME="gpu-jupyter-docker"
+
+# check container running
+if [ "$(docker ps -aq -f status=running -f name=$CONTAINER_NAME)" ]; then
+    echo "Container is already running : $CONTAINER_NAME"
+    exit 0
+elif [ "$(docker ps -q -f status=exited -f name=$CONTAINER_NAME)" ]; then
+    echo "Exited container found : $CONTAINER_NAME"
+    echo "Removing container..."
+    docker rm $CONTAINER_NAME
+fi
+
+# run container
+docker run --name $CONTAINER_NAME --gpus all -d -it -p 8848:8888 -v $(pwd)/work:/home/jovyan/work -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --user root cschranz/gpu-jupyter:v1.7_cuda-12.2_ubuntu-22.04
+
