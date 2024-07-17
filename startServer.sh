eab774a1a349c9a873f2d9a9890d38d6bf77f1c5
Jihwan Jang <jjh0302@gmail.com>
2024-07-18 00:30:38 +0900

    add --shm-size=4G

--- startServer.sh
+++ startServer.sh
@@ -17,5 +17,5 @@ elif [ "$(docker ps -q -f status=exited -f name=$CONTAINER_NAME)" ]; then
 fi
 
 # run container
-docker run --name $CONTAINER_NAME --gpus all -d -it -p 8848:8888 -v $(pwd)/work:/home/jovyan/work -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --user root cschranz/gpu-jupyter:v1.7_cuda-12.2_ubuntu-22.04
+docker run --name $CONTAINER_NAME --shm-size=4G --gpus all -d -it -p 8848:8888 -v $(pwd)/work:/home/jovyan/work -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --user root cschranz/gpu-jupyter:v1.7_cuda-12.2_ubuntu-22.04
 
