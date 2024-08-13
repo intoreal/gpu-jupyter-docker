004e789422ceb1c824ec2078b851b6570458b894
Jihwan jang <jjh0302@gmail.com>
2024-08-13 23:43:15 +0900

    make site-package folder for pip module storage

---
+++ makePackageFolder.sh
@@ -0,0 +1,1 @@
+docker cp gpu-jupyter-docker:/opt/conda/lib/python3.11/site-packages ./site-packages
---
+++ startServerWithPackage.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# check work folder
+[ ! -d "work" ] && mkdir work
+
+# declare container name
+CONTAINER_NAME="gpu-jupyter-docker"
+
+# check container running
+if [ "$(docker ps -aq -f status=running -f name=$CONTAINER_NAME)" ]; then
+    echo "Container is already running : $CONTAINER_NAME"
+    exit 0
+elif [ "$(docker ps -q -f status=exited -f name=$CONTAINER_NAME)" ]; then
+    echo "Exited container found : $CONTAINER_NAME"
+    echo "Removing container..."
+    docker rm $CONTAINER_NAME
+fi
+
+# run container
+docker run --name $CONTAINER_NAME --shm-size=4G --gpus all -d -it -p 8848:8888 -v $(pwd)/work:/home/jovyan/work -v $(pwd)/site-packages:/opt/conda/lib/python3.11/site-packages -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --user root cschranz/gpu-jupyter:v1.7_cuda-12.2_ubuntu-22.04
+
